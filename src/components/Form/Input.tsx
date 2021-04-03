154266c9bf227ce345487d4475c4147de5e216d2
Jefferson <jeffersonbraster@gmail.com>
2021-04-02 21:22:16 -0300

    criando component imput

---
+++ src/components/Form/Input.tsx
@@ -0,0 +1,29 @@
+import {
+  Input as ChackraInput,
+  FormLabel,
+  FormControl,
+  InputProps as ChacraInputprops,
+} from "@chakra-ui/react";
+
+interface InputProps extends ChacraInputprops {
+  name: string;
+  label?: string;
+}
+
+export function Input({ name, label, ...rest }: InputProps) {
+  return (
+    <FormControl>
+      {!!label && <FormLabel htmlFor={name}>{label}</FormLabel>}
+      <ChackraInput
+        id={name}
+        name={name}
+        focusBorderColor="pink.500"
+        bgColor="gray.900"
+        variant="filled"
+        _hover={{ bgColor: "gray.900" }}
+        size="lg"
+        {...rest}
+      />
+    </FormControl>
+  );
+}
