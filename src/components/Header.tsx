75426fd7fb984a1ac99a153101f13d0a72384e62
Jefferson <jeffersonbraster@gmail.com>
2021-04-02 21:37:38 -0300

    criando o header

---
+++ src/components/Header.tsx
@@ -0,0 +1,48 @@
+import { Flex, Text, Input, Icon } from "@chakra-ui/react";
+import { RiSearch2Line } from "react-icons/ri";
+
+export function Header() {
+  return (
+    <Flex
+      as="header"
+      w="100%"
+      maxWidth={1480}
+      h="20"
+      mx="auto"
+      mt="4"
+      align="center"
+      px="6"
+    >
+      <Text fontSize="3xl" fontWeight="bold" letterSpacing="tight" w="64">
+        Jeje.Dash
+        <Text as="span" ml="1" color="pink.500">
+          .
+        </Text>
+      </Text>
+
+      <Flex
+        as="label"
+        flex="1"
+        py="4"
+        px="8"
+        ml="6"
+        maxWidth={400}
+        alignSelf="center"
+        color="gray.200"
+        position="relative"
+        bg="gray.800"
+        borderRadius="full"
+      >
+        <Input
+          color="gray.50"
+          variant="unstyled"
+          placeholder="Buscar na plataforma"
+          px="4"
+          mr="4"
+          _placeholder={{ color: "gray.400" }}
+        ></Input>
+        <Icon as={RiSearch2Line} fontSize="20" />
+      </Flex>
+    </Flex>
+  );
+}
