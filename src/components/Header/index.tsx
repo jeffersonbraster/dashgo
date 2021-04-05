877b18545d273b3db8d43c9f647fbc95acce38e0
Jefferson <jeffersonbraster@gmail.com>
2021-04-04 22:29:00 -0300

    deixando o sidebar responsivo

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,4 +1,6 @@
-import { Flex, useBreakpointValue } from "@chakra-ui/react";
+import { Flex, Icon, IconButton, useBreakpointValue } from "@chakra-ui/react";
+import { RiMenuLine } from "react-icons/ri";
+import { useSidebarDrawer } from "../../context/SidebarDrawerContext";
 
 import { Logo } from "./Logo";
 import { NotificationNav } from "./NotificationNav";
@@ -6,6 +8,8 @@ import { Profile } from "./Profiile";
 import { SearchBox } from "./SearchBox";
 
 export function Header() {
+  const { onOpen } = useSidebarDrawer();
+
   const isWideVersion = useBreakpointValue({
     base: false,
     lg: true,
@@ -22,6 +26,16 @@ export function Header() {
       align="center"
       px="6"
     >
+      {!isWideVersion && (
+        <IconButton
+          icon={<Icon as={RiMenuLine} />}
+          mr="2"
+          fontSize="24"
+          variant="unstyled"
+          onClick={onOpen}
+          aria-label="Open navigation"
+        ></IconButton>
+      )}
       <Logo />
 
       {isWideVersion && <SearchBox />}
---
+++ src/context/SidebarDrawerContext.tsx
@@ -0,0 +1,28 @@
+import { useDisclosure, UseDisclosureReturn } from "@chakra-ui/hooks";
+import { useRouter } from "next/router";
+import { createContext, useContext, useEffect } from "react";
+
+interface SidebarDrawerContextProps {
+  children: React.ReactNode;
+}
+
+type SidebarDrawerContextData = UseDisclosureReturn;
+
+const SidebarDrawerContext = createContext({} as SidebarDrawerContextData);
+
+export function SidebarDrawerProvider({ children }: SidebarDrawerContextProps) {
+  const disclosure = useDisclosure();
+  const router = useRouter();
+
+  useEffect(() => {
+    disclosure.onClose();
+  }, [router.asPath]);
+
+  return (
+    <SidebarDrawerContext.Provider value={disclosure}>
+      {children}
+    </SidebarDrawerContext.Provider>
+  );
+}
+
+export const useSidebarDrawer = () => useContext(SidebarDrawerContext);
