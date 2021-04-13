cb06a5ef124f35b06683b7ec08078432185dc926
Jefferson <jeffersonbraster@gmail.com>
2021-04-13 11:10:07 -0300

    trocando o fetch pelo axios

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -22,13 +22,13 @@ import { Sidebar } from "../../components/Sidebar";
 import Link from "next/link";
 import { useEffect } from "react";
 import { useQuery } from "react-query";
+import { api } from "../../services/api";
 
 export default function UserList() {
-  const { data, isLoading, error } = useQuery(
+  const { data, isLoading, isFetching, error } = useQuery(
     "users",
     async () => {
-      const response = await fetch("http://localhost:3000/api/users");
-      const data = await response.json();
+      const { data } = await api.get("users");
 
       const users = data.users.map((user) => {
         return {
@@ -72,6 +72,9 @@ export default function UserList() {
           <Flex mb="8" justify="space-between" align="center">
             <Heading size="lg" fontWeight="normal">
               Usuários
+              {!isLoading && isFetching && (
+                <Spinner size="sm" ml="4" color="pink.500" />
+              )}
             </Heading>
             <Link href="/users/create" passHref>
               <Button
